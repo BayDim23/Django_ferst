f176392d0166ed4bb5f570df6af33f018b423008
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-08-21 09:17:14 +0300

    add new code

--- Zerro/main/urls.py
+++ Zerro/main/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
-]
-
-urlpatterns = [
-    path('data', views.data)
-]
-
-urlpatterns = [
+    path('', views.index),
+    path('data', views.data),
     path('test', views.test)
 ]
