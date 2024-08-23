7422f8c4e88a984d966531361ccca48197f41ab2
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-08-23 10:51:48 +0300

    add new code

--- Zerro/main/urls.py
+++ Zerro/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('data', views.data),
-    path('test', views.test)
+    path('', views.index, name='home'),
+    path('new', views.new, name='page2'),
+
 ]
--- Zerro/main/views.py
+++ Zerro/main/views.py
@@ -4,8 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
 
-def data(request):
+def new(request):
     return HttpResponse("<h1>Я вас приветствую</h1>")
 
-def test(request):
-    return HttpResponse("<h1>Мир во всем Мире</h1>")
