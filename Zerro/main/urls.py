80222097305d57e299b019a55ef14f4a850d36bf
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-08-25 11:43:44 +0300

    new code

--- Zerro/main/urls.py
+++ Zerro/main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('new', views.new, name='page2'),
+    path('m', views.m, name='m'),
+    path('s', views.s, name='s'),
+    path('f', views.f, name='f'),
+    path('g', views.g, name='g'),
 
 ]
--- Zerro/main/views.py
+++ Zerro/main/views.py
@@ -1,9 +1,20 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+	return render(request, 'main/index.html')
 
 def new(request):
-    return HttpResponse("<h1>Я вас приветствую</h1>")
+    return render(request, 'main/new.html')
+def m(request):
+    return render(request, 'main/m.html')
+
+def s(request):
+    return render(request, 'main/file/s.html')
+
+def f(request):
+    return render(request, 'main/file/f.html')
+
+def g(request):
+    return render(request, 'main/file/g.html')
 
