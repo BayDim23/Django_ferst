b74c99c4da2243cdd24ea26b7d5d157daea0f2f8
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-08-26 18:24:34 +0300

    new code

--- Zerro/news/views.py
+++ Zerro/news/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from .models import News_post
+from .forms import News_postForm
 
 
 
 def home(request):
     news = News_post.objects.all()
     return render(request, 'news/news.html', {'news': news})
+
+def create_news(request):
+    error = ''
+    if request.method == 'POST':
+        form = News_postForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        else:
+            error = 'Форма заполнена неверно'
+    form = News_postForm()
+    return render(request, 'news/add_news.html', {'form': form, 'error': error})
